c37af5de90775c6d9787f7e88ab5c324103f838a
Василий Сотников <alexisawtooth@gmail.com>
2022-07-22 00:32:42 +0300

    added default processor to items

--- perfume/items.py
+++ perfume/items.py
@@ -1,6 +1,6 @@
 import re, scrapy, unidecode
 from scrapy.exceptions import DropItem
-from itemloaders.processors import Compose, Identity
+from itemloaders.processors import MapCompose, TakeFirst
 
 
 def clean_tags(string):
@@ -19,10 +19,13 @@ def normalize_string(string):
 
 
 class PerfumeItem(scrapy.Item):
-    brand = scrapy.Field(input_processor = Compose(normalize_string), output_processor = Identity())
-    name_var = scrapy.Field(input_processor = Compose(clean_tags, normalize_string), output_processor = Identity())
+
+    default_output_processor = TakeFirst()
+
+    brand = scrapy.Field(input_processor = MapCompose(normalize_string))
+    name_var = scrapy.Field(input_processor = MapCompose(clean_tags, normalize_string))
     lilial = scrapy.Field()
     spf = scrapy.Field()
-    price_eu = scrapy.Field(input_processor = Compose(float), output_processor = Identity())
-    ean = scrapy.Field(input_processor = Compose(str), output_processor = Identity())
+    price_eu = scrapy.Field(input_processor = MapCompose(float))
+    ean = scrapy.Field(input_processor = MapCompose(str))
     url = scrapy.Field()
